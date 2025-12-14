df399917679ec5f5f9581bab582cf42102bc1513
codecode2121-droid <codecode2121@gmail.com>
2025-12-14 16:48:47 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=["start"])
+async def start(message: types.Message):
+    await message.answer("Привет! Я твой бот.")
+
+@dp.message_handler(commands=["hello"])
+async def hello(message: types.Message):
+    await message.answer("Hello, friend!")
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
